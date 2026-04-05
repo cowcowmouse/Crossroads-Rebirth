dcd8e2dfc9d112d9338e4210aa73f2270801b87f
Qs11s <3116137570@qq.com>
2026-04-05 17:12:32 +0800

    feat: point main dialog button to old_nail stage_1 timeline

--- project/scenes/main/button.gd
+++ project/scenes/main/button.gd
@@ -2,7 +2,7 @@ extends Button
 
 func _ready():
 	pressed.connect(_on_button_pressed)
-	
+
 func _on_button_pressed():
-	print("🔥 按钮点到了！准备启动对话: old_nail")
-	Dialogic.start("old_nail")
+	print("按钮点到了，准备启动对话: stage_1")
+	Dialogic.start("stage_1")
